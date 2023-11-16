176d9e9b93160144661af9f5116ed3fce9e0e90e
chris-piwinsky <chrispiwinsky@yahoo.com>
2023-11-16 12:48:19 -0600

    twitter logo and more podcast links

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -114,7 +114,7 @@ const Navbar: React.FC = () => {
             <a href={instagramLink} target="_blank" rel="noopener noreferrer" className='py-5 hover-red-text'>
               <Instagram size={25} />
             </a>
-            <a href={twitterLink} target="_blank" rel="noopener noreferrer" className='py-5 hover-red-text>
+            <a href={twitterLink} target="_blank" rel="noopener noreferrer" className='py-5 hover-red-text'>
               <Twitter size={25} />
             </a>
           </div>
